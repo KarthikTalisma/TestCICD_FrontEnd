f4b81f98d4e78d15cbc9774087113269639bbfa2
KarthikTalisma <karthikeyans@talisma.com>
2025-09-30 02:09:36 +0530

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import GaugeComponent from 'react-gauge-component';
 import './App.css'
 import { DataGrid } from '@mui/x-data-grid';
